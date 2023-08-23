823625340fd1902042b8483522297e7af3011506
Kavyaj554 <kavyaj554@gmail.com>
2023-08-23 22:05:17 +0530

    third commit

--- server.py
+++ server.py
@@ -22,5 +22,5 @@ def subtract():
     d = Result(data["first"] - data["second"])
     return jsonify(d)
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
